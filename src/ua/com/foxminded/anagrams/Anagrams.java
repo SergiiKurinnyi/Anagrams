77abb8016d2246d8ec12af08f5b55ee965fce4c4
Sergii Kurinnyi <se.kurinnyi@gmail.com>
2020-12-30 16:50:50 +0100

    code ready for a code review

---
+++ src/ua/com/foxminded/anagrams/Anagrams.java
@@ -0,0 +1,41 @@
+package ua.com.foxminded.anagrams;
+
+public class Anagrams {
+
+	public static void main(String[] args) {
+		System.out.println(reverseWords("a1bcd efg!h"));
+	}
+	
+	public static String reverseWords(String input) {
+		String[] words = input.split(" ");
+		StringBuilder reversedInput = new StringBuilder();
+		
+		for (int i = 0; i < words.length; i++) {
+			char[] wordChars = words[i].toCharArray();
+			for (int j = 0, k = wordChars.length - 1; j <= (wordChars.length / 2); j++) {
+				if (!isLetter(wordChars[k])) {
+					k--;
+				}
+				if (isLetter(wordChars[j]) && (j < k)) {
+					char tempChar;
+					tempChar = wordChars[k];
+					wordChars[k] = wordChars[j];
+					wordChars[j] = tempChar;
+					k--;
+				}
+			}
+			reversedInput.append(wordChars);
+			reversedInput.append(" ");
+		}
+		return reversedInput.toString().trim();
+	}
+	
+	public static boolean isLetter(char c) {
+		byte inputCharByte = (byte) c;
+		if ((inputCharByte > 64 && inputCharByte < 91) || (inputCharByte > 96 && inputCharByte < 123)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
